762f9342006ac75a78c635bfa45a49a1cda00bf1
HUMILITY001 <humilityobarue@Gmail.com>
2024-09-20 02:47:26 -1100

    save data and generated HTML for delivery option

--- data/cart.js
+++ data/cart.js
@@ -4,27 +4,20 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if(!cart){
     cart = [{
         productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity : 2
+        quantity : 2,
+        deliveryOptionId: '1'
     },{
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '2'
     }];
 }
 
 
-const getCarts = () =>{
+/*const getCarts = () =>{
     return JSON.parse(window.localStorage.getItem('cart'))
-  }
+  }*/
   
-//   const updateCheckoutHeader = () => {
-//     const carts = getCarts()
-//     console.log('called')
-//     const header = document.getElementById('count-parent')
-//     console.log(header)
-
-//     console.log(carts.length)
-//     header.innerHTML = `${carts?.length} item(s)`
-//   }
 
 
  function saveToStorage(){
@@ -51,7 +44,8 @@ export function addToCart(productId){
         }else{
             cart.push({
                 productId: productId,
-                quantity: quantity
+                quantity: quantity,
+                deliveryOptionId: '1'
             });
         }
         saveToStorage();
